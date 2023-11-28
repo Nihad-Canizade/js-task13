16d374ff4cc3601f1e19fa291b39c848d9bbda06
Nihad-Canizade <nihadcanizade000@gmail.com>
2023-11-28 10:35:49 +0400

    js& 'c:\Users\NIHAD DEV\Desktop\js-task13\script.js' clear
    clear

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Container 1
-let container = document.createElement("div");
-container.setAttribute("class", "container");
+let container1 = document.createElement("div");
+container1.setAttribute("class", "container1");
 
 // Navbar
 let nav = document.createElement("nav");
-let navin = document.createElement("div");
 let ul = document.createElement("ul");
 let li1 = document.createElement("li");
 let li2 = document.createElement("li");
@@ -12,11 +11,10 @@ let li3 = document.createElement("li");
 let li4 = document.createElement("li");
 let navp1 = document.createElement("p");
 
-document.body.append(nav);
-nav.append(container);
-container.append(navin);
-navin.append(navp1);
-navin.append(ul);
+document.body.append(container1);
+container1.append(nav);
+nav.append(navp1);
+nav.append(ul);
 ul.append(li1, li2, li3, li4);
 
 li1.innerText = "Home";
@@ -25,18 +23,12 @@ li3.innerText = "Projects";
 li4.innerText = "Contact";
 navp1.innerText = "StartBootstrap";
 
-navin.setAttribute("class", "nav-in");
 
-
-
-
-navin.style.display = "flex";
-navin.style.justifyContent = "space-between";
-navin.style.alignItems = "center";
-navin.style.paddingTop = "20px";
-navin.style.paddingRight = "10%";
-navin.style.paddingLeft = "10%";
-navin.style.paddingBottom = "20px";
+nav.style.display = "flex";
+nav.style.justifyContent = "space-between";
+nav.style.alignItems = "center";
+nav.style.paddingTop = "20px";
+nav.style.paddingBottom = "20px";
 
 li1.style.listStyle = "none";
 li1.style.display = "inline-block";
@@ -75,7 +67,6 @@ navp1.style.cursor = "pointer";
 
 // Section1
 let section1 = document.createElement("section");
-let sec1in = document.createElement("div");
 let fortext = document.createElement("div");
 let forimg = document.createElement("div");
 let secp1 = document.createElement("p");
@@ -86,13 +77,11 @@ let btn1 = document.createElement("button");
 let btn2 = document.createElement("button");
 let img = document.createElement("img");
 
-document.body.append(section1);
-section1.append(container);
-container.append(sec1in);
-sec1in.append(fortext);
+container1.append(section1);
+section1.append(fortext);
 fortext.append(secp1, secp2, secp3, secp4);
 fortext.append(btn1, btn2);
-sec1in.append(forimg);
+section1.append(forimg);
 forimg.append(img);
 
 secp1.innerText = "DESIGN · DEVELOPMENT · MARKETING";
@@ -103,7 +92,6 @@ btn1.innerText = "Resume";
 btn2.innerText = "Projects";
 
 section1.setAttribute("class", "section1");
-sec1in.setAttribute("class", "sec1-in");
 secp1.setAttribute("class", "sec1-p1");
 secp2.setAttribute("class", "sec1-p2");
 secp3.setAttribute("class", "sec1-p3");
@@ -117,16 +105,19 @@ img.classList.add("image");
 
 // Section 2
 let section2 = document.createElement("section");
-let sec2in = document.createElement("div");
 let sec2h1 = document.createElement("h1");
 let sec2p1 = document.createElement("p");
 let sec2p2 = document.createElement("p");
 let sec2p3 = document.createElement("p");
 let sec2p4 = document.createElement("p");
+let icons = document.createElement("div");
+let i1 = document.createElement("i");
+let i2 = document.createElement("i");
+let i3 = document.createElement("i");
 
 document.body.append(section2);
-section2.append(sec2in);
-sec2in.append(sec2h1, sec2p1, sec2p2, sec2p3, sec2p4);
+section2.append(sec2h1, sec2p1, sec2p2, sec2p3, sec2p4, icons);
+icons.append(i1, i2, i3);
 
 sec2h1.innerText = "About Me";
 sec2p1.innerText = "My name is Start Bootstrap and I help brands grow.";
@@ -135,12 +126,45 @@ sec2p3.innerText = "consequatur autem. Eveniet quasi nobis aliquid cumque offici
 sec2p4.innerText = "atque dolorem?";
 
 section2.classList.add("section2");
-sec2in.classList.add("sec2-in");
 sec2h1.classList.add("sec2-h1");
 sec2p1.classList.add("sec2-p1");
 sec2p2.classList.add("sec2-p2");
 sec2p3.classList.add("sec2-p3");
 sec2p4.classList.add("sec2-p4");
+icons.classList.add("icons")
+i1.setAttribute("class", "bi bi-twitter");
+i2.setAttribute("class", "bi bi-linkedin");
+i3.setAttribute("class", "bi bi-github");
+
+// Footer
+let footer = document.createElement("footer");
+let fp1 = document.createElement("p");
+let foul = document.createElement("ul");
+let fli1 = document.createElement("li");
+let fli2 = document.createElement("li");
+let fli3 = document.createElement("li");
+
+// Container 2
+let container2 = document.createElement("div");
+container2.setAttribute("class", "container2");
+
+document.body.append(container2);
+container2.append(footer);
+footer.append(fp1);
+footer.append(foul);
+foul.append(fli1, fli2, fli3);
+
+fp1.innerText = "Copyright © Your Website 2023";
+fli1.innerText = "Privacy";
+fli2.innerText = "Terms";
+fli3.innerText = " Contact";
+
+footer.classList.add("footer");
+fp1.classList.add("f-p1");
+fli1.classList.add("f-li1");
+fli2.classList.add("f-li2");
+fli3.classList.add("f-li3");
+
 
 
 
